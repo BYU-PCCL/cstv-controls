7974c6f19da53d252a4a22e618b107252e1efde3
Vin Howe <24789592+vinhowe@users.noreply.github.com>
2021-06-29 11:20:38 -0600

    Add PageWidth wrapper component

---
+++ src/common/PageWidth.tsx
@@ -0,0 +1,23 @@
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
+import React, { PropsWithChildren, ReactNode } from "react";
+
+const pageWrapperStyle = css`
+  display: flex;
+  justify-content: center;
+`;
+
+// TODO: Decide if this is the right max width based on Figma designs
+const pageWidthStyle = css`
+  width: 600px;
+`;
+
+const PageWidth = ({ children }: { children?: ReactNode }): JSX.Element => {
+  return (
+    <main css={pageWrapperStyle}>
+      <div css={pageWidthStyle}>{children}</div>
+    </main>
+  );
+};
+
+export default PageWidth;

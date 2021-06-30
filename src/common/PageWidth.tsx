827a54694b3b756e37a5ad9aeee81ff3b9b4fa44
Vin Howe <24789592+vinhowe@users.noreply.github.com>
2021-06-29 19:38:54 -0600

    Make PageWidth 100% height

--- src/common/PageWidth.tsx
+++ src/common/PageWidth.tsx
@@ -5,11 +5,13 @@ import React, { ReactNode } from "react";
 const pageWrapperStyle = css`
   display: flex;
   justify-content: center;
+  height: 100%;
 `;
 
 // TODO: Decide if this is the right max width based on Figma designs
 const pageWidthStyle = css`
   width: 100%;
+  height: 100%;
 
   @media (min-width: 600px) {
     width: 600px;

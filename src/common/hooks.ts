cfde1c38b8ddb8f0554162f0af7bfa9ad805ea3b
Vin Howe <24789592+vinhowe@users.noreply.github.com>
2021-07-01 14:51:07 -0600

    Debounce useCurrentExperience hook to prevent flashing

---
+++ src/common/hooks.ts
@@ -0,0 +1,24 @@
+import { useEffect, useState } from "react";
+
+// Based on https://usehooks.com/useDebounce/ from the Unlicense-licensed
+// useHooks project
+export const useDebounce = <T>(value: T, delay: number): T => {
+  // State and setters for debounced value
+  const [debouncedValue, setDebouncedValue] = useState(value);
+  useEffect(
+    () => {
+      // Update debounced value after delay
+      const handler = setTimeout(() => {
+        setDebouncedValue(value);
+      }, delay);
+      // Cancel the timeout if value changes (also on delay change or unmount)
+      // This is how we prevent debounced value from updating if value is changed ...
+      // .. within the delay period. Timeout gets cleared and restarted.
+      return () => {
+        clearTimeout(handler);
+      };
+    },
+    [value, delay] // Only re-call effect if value or delay changes
+  );
+  return debouncedValue;
+};

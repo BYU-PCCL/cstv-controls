f785609635584cc06fa2b6eae4a4e0ccb05f2d1c
Vin Howe <24789592+vinhowe@users.noreply.github.com>
2022-05-03 16:39:04 -0600

    Ignore warnings for statically unused VideoScrubber

--- src/controls/generated/index.ts
+++ src/controls/generated/index.ts
@@ -1,5 +1,7 @@
-import Controls1 from "./life";
-import VideoScrubber from "../VideoScrubber";
+import Controls1 from "./life/index";
+// VideoScrubber will be used by modules that are imported dynamically later
+// noinspection ES6UnusedImports
+import VideoScrubber from "../VideoScrubber"; // eslint-disable-line @typescript-eslint/no-unused-vars
 
 const controls: Map<string, () => JSX.Element> = new Map([["life", Controls1]]);
 

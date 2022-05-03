524d25e9959c6c11339080826e3bada556594583
Vin Howe <24789592+vinhowe@users.noreply.github.com>
2022-05-03 16:41:28 -0600

    Add plain JS controls example

---
+++ src/controls/generated/life/index-js-example.js
@@ -0,0 +1,52 @@
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
+import React, { useCallback, useState } from "react";
+import { useMessaging } from "@footron/controls-client";
+import { Slider } from "@material-ui/core";
+
+const containerStyle = css`
+  padding: 16px;
+  overflow-x: hidden;
+
+  p {
+    margin: 0 0 16px;
+  }
+`;
+
+const ControlsComponent = () => {
+  const [number, setNumber] = useState();
+
+  const { sendMessage } = useMessaging((message) => {
+    setNumber(message);
+  });
+
+  const updateSlider = useCallback(
+    async (event, value) => {
+      await sendMessage(value);
+    },
+    [sendMessage]
+  );
+
+  return (
+    <div css={containerStyle}>
+      <p>
+        <b>Move the slider!</b>
+      </p>
+      <Slider
+        min={0}
+        max={1}
+        onChange={updateSlider}
+        step={0.05}
+        marks
+        defaultValue={0}
+      />
+      <br />
+      <br />
+      <div style={{ fontFamily: "monospace", fontSize: "20pt" }}>
+        {number || "Loading data..."}
+      </div>
+    </div>
+  );
+};
+
+export default ControlsComponent;

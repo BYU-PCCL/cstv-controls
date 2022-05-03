4302026aa6d66e0cf1a4a228aa9a014cf6511e68
Vin Howe <24789592+vinhowe@users.noreply.github.com>
2022-05-03 16:39:27 -0600

    Switch to React 18's createRoot

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,18 @@
 import React from "react";
-import ReactDOM from "react-dom";
 import "./index.css";
 import FootronControlsApp from "./FootronControlsApp";
+import { createRoot } from "react-dom/client";
 // import reportWebVitals from "./reportWebVitals";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <FootronControlsApp />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+const container = document.getElementById("root");
+if (container) {
+  const root = createRoot(container);
+  root.render(
+    <React.StrictMode>
+      <FootronControlsApp />
+    </React.StrictMode>
+  );
+}
 
 // @vinhowe: We may want to report performance monitoring in the future
 // to an analytics service like Google Analytics or Mixpanel
